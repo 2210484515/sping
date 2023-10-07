f91cd5b2044a6e24a581e3969cc50acd9ff8ec16
yueyang <2210484515@qq.com>
2023-10-07 10:34:38 +0800

    修改启动日志，增加/user-service上下文，logback 日志

--- share-user/src/main/java/yy/example/share/user/config/UserApplication.java
+++ share-user/src/main/java/yy/example/share/user/config/UserApplication.java
@@ -1,14 +1,21 @@
 package yy.example.share.user.config;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
 
 @SpringBootApplication
 @ComponentScan("yy.example")
+@Slf4j
 public class UserApplication {
     public static void main(String[] args) {
-        SpringApplication.run(UserApplication.class,args);
+        SpringApplication app=new SpringApplication(UserApplication.class);
+        Environment env= app.run(args).getEnvironment();
+        log.info("启动成功！！");
+        log.info("测试地址: http://127.0.0.1:{}{}/hello", env.getProperty("server.port"), env.getProperty("server.servlet.context-path"));
+
     }
 }
