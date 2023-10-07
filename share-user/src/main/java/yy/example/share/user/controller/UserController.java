ffeb813ef98df877623fda6f0283e37e736e6747
yueyang <2210484515@qq.com>
2023-10-07 16:08:50 +0800

    统一返回结果

--- share-user/src/main/java/yy/example/share/user/controller/UserController.java
+++ share-user/src/main/java/yy/example/share/user/controller/UserController.java
@@ -2,6 +2,7 @@ package yy.example.share.user.controller;
 
 import jakarta.annotation.Resource;
 import org.springframework.web.bind.annotation.*;
+import yy.example.share.common.resp.CommonResp;
 import yy.example.share.user.domain.dto.LoginDTO;
 import yy.example.share.user.domain.entity.User;
 import yy.example.share.user.service.UserService;
@@ -12,11 +13,17 @@ public class UserController {
     @Resource
     private UserService userService;
     @GetMapping("/count")
-    public Long count(){
-        return userService.count();
+    public CommonResp<Long> count(){
+        Long count= userService.count();
+        CommonResp<Long> commonResp=new CommonResp<>();
+        commonResp.setData(count);
+        return commonResp;
     }
     @PostMapping("/login")
-    public User login(@RequestBody LoginDTO loginDTO){
-        return userService.login(loginDTO);
+    public CommonResp<User> login(@RequestBody LoginDTO loginDTO){
+        User user=userService.login(loginDTO);
+        CommonResp<User> commonResp=new CommonResp<>();
+        commonResp.setData(user);
+        return commonResp;
     }
 }
